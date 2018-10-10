be67a588776c6e3871d0c74df0f7e6a79b5de712
oke vese <okevese@gmail.com>
2018-10-10 18:51:21 +0100

    implemented login middleware

--- app_api/middleware/authentication.js
+++ app_api/middleware/authentication.js
@@ -28,11 +28,24 @@ const register = (req, res, next) => {
 }
 
 
-const login = (req, res, next) => {
+const login = (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     return res.status(412).json({ errors: errors.array() });
   } 
+
+  passport.authenticate('local', (err, user, info) => {
+    if (err) return next(err);
+
+    if(user) {
+      const token = user.generateJwt();
+      sendJsonResponse(res, 200, {
+        "token": token
+      });
+    } else {
+      sendJsonResponse(res, 401, info);
+    }
+  })(req, res, next); // Makes `req, res, next` available to passport
 }
 
 
