4737cb8773e7496536248095e1d686d4668e0617
oke vese <okevese@gmail.com>
2018-11-04 14:51:24 +0100

    saved daily clorie meals to database

--- app_api/middleware/recipeDailyCalorie.js
+++ app_api/middleware/recipeDailyCalorie.js
@@ -1,7 +1,21 @@
 
-// custom modification of request body, database operations and whatnot
+import mongoose from 'mongoose';
+import RecipeDailyCalorie from '../models/RecipeDailyCalorie';
+
 const recipeDailyCalorie = (req, res, next) => {
-  res.status(200).send(res.locals.dailyCalorie);
+  const dailyCalorie = new RecipeDailyCalorie({
+    meals: res.locals.dailyCalorie.meals,
+    nutrients: res.locals.dailyCalorie.nutrients
+  });
+
+  dailyCalorie
+    .save()
+    .then(recipeDailyCalorie => {
+      res.status(200).json({ message: "saved success", recipeDailyCalorie });
+    })
+    .catch((e) => {
+      next(e);
+    });
 }
 
 export default recipeDailyCalorie;
