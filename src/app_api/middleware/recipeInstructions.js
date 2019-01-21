e06ad156919b2d2cc8f183955569bb1fb3f2f865
oke vese <okevese@gmail.com>
2019-01-21 19:29:18 +0100

    removed unused code

--- src/app_api/middleware/recipeInstructions.js
+++ src/app_api/middleware/recipeInstructions.js
@@ -1,10 +1,3 @@
-// custom modification of request body, database operations and whatnot
-//const recipeInstructions = (req, res, next) => {
-  //res.status(200).send(res.locals.recipeInfo);
-//}
-
-//export default recipeInstructions;
-
 
 import mongoose from 'mongoose';
 import RecipeInstructions from '../models/RecipeInstructions';
