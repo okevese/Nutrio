2afa8a02ef6d64e9b02cf6b295e4dab0561b74b2
oke vese <okevese@gmail.com>
2019-01-28 09:27:47 +0100

    configured routes

--- src/app_client/app.js
+++ src/app_client/app.js
@@ -1,1 +1,12 @@
-angular.module('nutrioApp', []);
+angular.module('nutrioApp', ['ngRoute']);
+
+function config($routeProvider) {
+  $routeProvider
+    .when('/', {
+    })
+    .otherwise({redirectTo: '/'});
+}
+
+angular
+  .module('nutrioApp')
+  .config(['$routeProvider', config]);
