76a694e4b321534b6f72f7953afa74cac0f435ef
oke vese <okevese@gmail.com>
2019-01-28 11:30:17 +0100

    set up services in homepage

---
+++ src/app_client/common/services/triviaData.service.js
@@ -0,0 +1,9 @@
+
+var triviaData = function($http) {
+  return $http.get('/api/v1/trivia');
+}
+
+
+angular
+  .module('nutrioApp')
+  .service('triviaData', triviaData);
--- src/app_client/home/home.controller.js
+++ src/app_client/home/home.controller.js
@@ -2,10 +2,19 @@ angular
   .module('nutrioApp')
   .controller('homeCtrl', homeCtrl);
 
-function homeCtrl () {
+function homeCtrl (triviaData) {
   var vm = this;
   vm.pageHeader = {
     title: 'Nutrio',
     strapline: 'Find diets for healthy living'
   };
+
+  vm.message = "Loading food trivia...";
+  triviaData
+    .then(function(trivia) {
+      if(trivia.data.text) vm.message = "";
+      vm.trivia = trivia.data.text;
+    }, function(e) {
+      console.log(e);
+    });
 }
