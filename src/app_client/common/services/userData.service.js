8a43cd09693c425d8541d52bc00b904283c4b34a
oke vese <okevese@gmail.com>
2019-05-15 18:08:52 +0100

    Write service to get a user's saved meals

--- src/app_client/common/services/userData.service.js
+++ src/app_client/common/services/userData.service.js
@@ -6,7 +6,16 @@
   userData.$inject = ['$http'];
 
   function userData($http) {
-    
+    var getMeals = function() {
+      return $http({
+        method: "GET",
+        url: "api/v1/meals"
+      })
+    };
+
+    return {
+      getMeals: getMeals
+    };
   }
 
 })();
