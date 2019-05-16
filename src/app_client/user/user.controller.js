0e708ac025c49647e9c55b470c6323b8492413a4
oke vese <okevese@gmail.com>
2019-05-16 21:25:40 +0100

    fetch data from service in the user controller

--- src/app_client/user/user.controller.js
+++ src/app_client/user/user.controller.js
@@ -3,14 +3,21 @@
     .module('nutrioApp')
     .controller('userCtrl', userCtrl);
 
-  function userCtrl() {
+  userCtrl.$inject = ['userData'];    
+  function userCtrl(userData) {
     var vm = this;
 
     vm.pageHeader = {
       title: "User Profile",
       strapline: "Track your favourite meals for later"
     };
-    
+
+    userData.getMeals()
+      .then(function(meals) {
+        console.log(meals);
+      }, function(e) {
+        console.log(e);
+      })
   }  
 
 })();
