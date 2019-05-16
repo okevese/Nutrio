0f7bd038ac72f8694a5ce0d66be53333ffba1696
oke vese <okevese@gmail.com>
2019-05-16 22:09:58 +0100

    display retrieved data in view

--- src/app_client/user/user.controller.js
+++ src/app_client/user/user.controller.js
@@ -11,10 +11,13 @@
       title: "User Profile",
       strapline: "Track your favourite meals for later"
     };
+    vm.message = "Loading saved meals...";
 
     userData.getMeals()
       .then(function(meals) {
         console.log(meals);
+        if(meals.data.meals[0].id) vm.message = "";
+        vm.meals = meals.data.meals;
       }, function(e) {
         console.log(e);
       })
